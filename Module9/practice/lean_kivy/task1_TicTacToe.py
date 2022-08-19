e2f33c875505b9460d2af2bdd8cb258c8963d792
Sergey Mikhaylenko <sergey.mikhaylenko@gmail.com>
2022-08-19 16:16:34 +0300

    Update task1_TicTacToe.py

--- Module9/practice/lean_kivy/task1_TicTacToe.py
+++ Module9/practice/lean_kivy/task1_TicTacToe.py
@@ -9,19 +9,21 @@ Window.size = (400, 500)
 
 
 class MyApp(App):
+    STEPS=["X","O","X","O","X","O","X","O","X"] # очередь ходов
+    STOP=False # флаг останова игры
     def build(self):
         grid = GridLayout(padding=15, cols=3)
         # GridLayout: https://kivy.org/doc/stable/api-kivy.uix.gridlayout.html
         # Widgets: https://kivy.org/doc/stable/api-kivy.uix.html
         self.cell1 = Button(on_press=self.on_btn_click, font_size=40)
-        self.cell2 = Button(font_size=40)
-        self.cell3 = Button(font_size=40)
-        self.cell4 = Button(font_size=40)
-        self.cell5 = Button(font_size=40)
-        self.cell6 = Button(font_size=40)
-        self.cell7 = Button(font_size=40)
-        self.cell8 = Button(font_size=40)
-        self.cell9 = Button(font_size=40)
+        self.cell2 = Button(on_press=self.on_btn_click, font_size=40)
+        self.cell3 = Button(on_press=self.on_btn_click, font_size=40)
+        self.cell4 = Button(on_press=self.on_btn_click, font_size=40)
+        self.cell5 = Button(on_press=self.on_btn_click, font_size=40)
+        self.cell6 = Button(on_press=self.on_btn_click, font_size=40)
+        self.cell7 = Button(on_press=self.on_btn_click, font_size=40)
+        self.cell8 = Button(on_press=self.on_btn_click, font_size=40)
+        self.cell9 = Button(on_press=self.on_btn_click, font_size=40)
         self.lbl_status = Label(text="Ходит: X")
         grid.add_widget(self.cell1)
         grid.add_widget(self.cell2)
@@ -35,10 +37,41 @@ class MyApp(App):
         grid.add_widget(self.lbl_status)
         return grid
 
-    def on_btn_click(self, instance):
-        instance.text = 'X'
-        self.lbl_status.text = "Ходит: O"
+    def on_btn_click(self,instance):
+        if MyApp.STOP:
+            self.lbl_status.text = 'Игра закончена'
+            return
+        if instance.text:
+            return 
+        instance.text = str(self.get_step())
+        if instance.text=="X":
+            step="O"
+        else:
+            step="X"
+        winer=self.is_winer("O")
+        if winer!="":
+            self.lbl_status.text = f'Победил: {winer} '
+            MyApp.STOP=True
+        winer=self.is_winer("X")
+        if winer!="":
+            self.lbl_status.text = f'Победил: {winer} '
+            MyApp.STOP=True
+        else:
+            self.lbl_status.text = f'Ходит: {step} '
 
+    def get_step(self):
+        return MyApp.STEPS.pop(0)
+
+    def is_winer(self,who):
+        if (self.cell1.text==who and self.cell2.text==who and self.cell3.text==who) or (self.cell4.text==who and self.cell5.text==who and self.cell6.text==who) or (self.cell7.text==who and self.cell8.text==who and self.cell9.text==who):
+           return who
+        if (self.cell1.text==who and self.cell4.text==who and self.cell7.text==who) or (self.cell2.text==who and self.cell5.text==who and self.cell8.text==who) or (self.cell3.text==who and self.cell6.text==who and self.cell9.text==who):
+           return who
+        if (self.cell1.text==who and self.cell5.text==who and self.cell9.text==who) or (self.cell3.text==who and self.cell5.text==who and self.cell6.text==who):
+           return who
+        return ""
+
+        return MyApp.STEPS.pop(0)
 
 # TODO: дана заготовка игры "Крестики-нолики". Закончите игру.
 # TODO: Когда кто-то выстраивает вряд/по диагонали кри крестика или три нолика, в поле self.lbl_status, вывести: "победил Х".
